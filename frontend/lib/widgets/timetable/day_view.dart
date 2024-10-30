17439bb54ceee6bbdfda1b1210837d585839f41f
adhitht <hello@adhitht.me>
2024-10-30 12:14:36 +0530

    refactor: Add date picker to DayViewScreen (DayHeader)

--- frontend/lib/widgets/timetable/day_view.dart
+++ frontend/lib/widgets/timetable/day_view.dart
@@ -2,17 +2,28 @@ import 'package:dashbaord/models/lecture_model.dart';
 import 'package:dashbaord/models/time_table_model.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar_view/calendar_view.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 
-DateTime get _now => DateTime.now();
-
-class DayViewScreen extends StatelessWidget {
+class DayViewScreen extends StatefulWidget {
   final Timetable? timetable;
+  final DateTime? initialDate;
 
   final BuildContext context;
 
   const DayViewScreen(
-      {super.key, required this.context, required this.timetable});
+      {super.key,
+      required this.context,
+      required this.timetable,
+      this.initialDate});
+
+  @override
+  State<DayViewScreen> createState() => _DayViewScreenState();
+}
+
+class _DayViewScreenState extends State<DayViewScreen> {
+  final key = GlobalKey<DayViewState>();
 
   List<CalendarEventData> convertTimetableToCalendarEvents(
       List<Lecture> lectures) {
@@ -64,7 +75,7 @@ class DayViewScreen extends StatelessWidget {
           events.add(CalendarEventData(
             date: date,
             title: lecture.courseCode,
-            description: timetable!.courses[lecture.courseCode]!,
+            description: widget.timetable!.courses[lecture.courseCode]!,
             startTime: startTime,
             endTime: endTime,
           ));
@@ -78,77 +89,121 @@ class DayViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<CalendarEventData> events =
-        convertTimetableToCalendarEvents(timetable!.slots);
+        convertTimetableToCalendarEvents(widget.timetable!.slots);
+    DateTime currentDate = widget.initialDate ?? DateTime.now();
 
-    return DayView(
-      controller: EventController()..addAll(events),
-      backgroundColor: Theme.of(context).canvasColor,
-      heightPerMinute: 1,
-      showVerticalLine: false,
-      initialDay: _now,
-      keepScrollOffset: true,
-      startDuration: Duration(hours: 8),
-      dayTitleBuilder: (date) {
-        return Container();
-      },
-      timeLineBuilder: (date) {
-        return Container(
-          width: 60,
-          alignment: Alignment.center,
-          child: Text(
-            DateFormat('h a').format(date),
-            style: TextStyle(color: Colors.grey),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 100,
+          child: DatePicker(
+            currentDate,
+            initialSelectedDate: currentDate,
+            selectionColor: Theme.of(context).cardColor,
+            selectedTextColor: Colors.redAccent,
+            deactivatedColor: Theme.of(context).textTheme.bodyMedium!.color!,
+            dateTextStyle: GoogleFonts.inter(
+              fontWeight: FontWeight.w700,
+              fontSize: 24,
+              color: Theme.of(context).textTheme.bodyLarge?.color,
+            ),
+            dayTextStyle: GoogleFonts.inter(
+              color: Theme.of(context).textTheme.bodyLarge?.color,
+              fontWeight: FontWeight.w500,
+            ),
+            monthTextStyle: GoogleFonts.inter(
+              fontWeight: FontWeight.w500,
+              color: Theme.of(context).textTheme.bodyLarge?.color,
+            ),
+            onDateChange: (date) {
+              key.currentState?.animateToDate(date);
+              setState(() {
+                currentDate = date;
+              });
+            },
           ),
-        );
-      },
-      eventTileBuilder: (date, events, rect, startTime, endTime) {
-        return Container(
-          margin: const EdgeInsets.symmetric(vertical: 1),
-          padding: endTime.difference(startTime) > Duration(minutes: 40)
-              ? EdgeInsets.all(8)
-              : EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          height: 80, // Set your desired height here
-          decoration: BoxDecoration(
-            color: Colors.blueAccent,
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                offset: Offset(0, 2),
-                blurRadius: 4,
-              ),
-            ],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                events[0].title,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Expanded(
+          child: DayView(
+            key: key,
+            controller: EventController()..addAll(events),
+            backgroundColor: Theme.of(context).canvasColor,
+            heightPerMinute: 1,
+            showVerticalLine: false,
+            initialDay: currentDate,
+            keepScrollOffset: true,
+            startDuration: Duration(hours: 8),
+            liveTimeIndicatorSettings: LiveTimeIndicatorSettings(
+              color: Theme.of(context).textTheme.bodyLarge!.color!,
+            ),
+            dayTitleBuilder: (date) {
+              return Container();
+            },
+            timeLineBuilder: (date) {
+              return Container(
+                width: 60,
+                alignment: Alignment.center,
+                child: Text(
+                  DateFormat('h a').format(date),
+                  style: TextStyle(color: Colors.grey),
                 ),
-              ),
-              if (events[0].description != null &&
-                  endTime.difference(startTime) > Duration(minutes: 40))
-                Text(
-                  events[0].description!,
-                  overflow: TextOverflow.fade,
-                  maxLines:
-                      endTime.difference(startTime) <= Duration(hours: 1)
-                          ? 1
-                          : 2,
-                  style: TextStyle(
-                    color: Colors.white70,
-                  ),
+              );
+            },
+            eventTileBuilder: (date, events, rect, startTime, endTime) {
+              return Container(
+                margin: const EdgeInsets.symmetric(vertical: 1),
+                padding: endTime.difference(startTime) > Duration(minutes: 40)
+                    ? EdgeInsets.all(8)
+                    : EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                height: 80, // Set your desired height here
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0, 2),
+                      blurRadius: 4,
+                    ),
+                  ],
                 ),
-            ],
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      events[0].title,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    if (events[0].description != null &&
+                        endTime.difference(startTime) > Duration(minutes: 40))
+                      Text(
+                        events[0].description!,
+                        overflow: TextOverflow.fade,
+                        maxLines:
+                            endTime.difference(startTime) <= Duration(hours: 1)
+                                ? 1
+                                : 2,
+                        style: TextStyle(
+                          color: Colors.white70,
+                        ),
+                      ),
+                  ],
+                ),
+              );
+            },
+            onEventTap: (event, date) {
+              debugPrint(Theme.of(context).canvasColor.toString());
+            },
           ),
-        );
-      },
-      onEventTap: (event, date) {
-        debugPrint(Theme.of(context).canvasColor.toString());
-      },
+        ),
+      ],
     );
   }
 }
